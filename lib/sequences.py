cf71bdd88b2ce71d2e83f9f46a158637d2e741e4
Steve Romero <romero.steve.a@gmail.com>
2023-07-08 11:30:03 -0700

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    f1 = 0
+    f2 = 1
+    count = 2
+    #special case for start of the list
+    if length > 0:
+        fib.append(0)
+    if length > 1:
+        fib.append(1)
+
+    while count < length:
+        count += 1
+        added = f1+f2
+        fib.append(added)
+        f1 = f2
+        f2 = added
+
+    print(fib)
